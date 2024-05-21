6f34024f2328e38449b3ea739d09a78bde2b82e8
xbmlz <997909544@qq.com>
2024-05-21 23:08:42 +0800

    feat: add log level methods

--- pkg/logger/level.go
+++ pkg/logger/level.go
@@ -1,5 +1,7 @@
 package logger
 
+import "strings"
+
 // A Level is a logging priority. Higher levels are more important.
 type Level int8
 
@@ -16,3 +18,33 @@ const (
 	// it shouldn't generate any error-level logs.
 	ErrorLevel
 )
+
+func (l Level) String() string {
+	switch l {
+	case DebugLevel:
+		return "DEBUG"
+	case InfoLevel:
+		return "INFO"
+	case WarnLevel:
+		return "WARN"
+	case ErrorLevel:
+		return "ERROR"
+	default:
+		return ""
+	}
+}
+
+// ParseLevel parses a level string into a logger Level value.
+func ParseLevel(s string) Level {
+	switch strings.ToUpper(s) {
+	case "DEBUG":
+		return DebugLevel
+	case "INFO":
+		return InfoLevel
+	case "WARN":
+		return WarnLevel
+	case "ERROR":
+		return ErrorLevel
+	}
+	return InfoLevel
+}
